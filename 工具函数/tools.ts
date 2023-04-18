5be1ad7076742fb4d4736ed00c27c51b2c5dd68c
zhaoyebaitu <122477559@qq.com>
2023-04-18 16:47:36 +0800

    feat: 工具函数更新

---
+++ 工具函数/tools.ts
@@ -0,0 +1,31 @@
+/**
+ * @description: 用来删除对象中不需要的属性，返回删除后对象的浅拷贝
+ * @param {T} obj
+ * @param {Array} keys
+ * @return {*}
+ */
+function omit<T extends Record<string | number, any>, K extends keyof T>(
+  obj: T,
+  keys: Array<K> // 类型 string 是为了某些未被声明的属性被omit，不加就限定传入 obj参数的属性名 union类型
+  // keys: Array<K | string> // 类型 string 是为了某些未被声明的属性被omit，不加就限定传入 obj参数的属性名 union类型
+): Omit<T, K> {
+  const clone: T = { ...obj };
+  keys.forEach((key) => {
+    if (key in clone) {
+      delete clone[key];
+    }
+  });
+
+  return clone;
+}
+
+// 筛选出对象中需要的属性
+function pick<T extends Record<string | number, any>, K extends keyof T>(obj: T, keys: Array<K>) {
+  const clone: Partial<Record<K, any>> = {};
+  keys.forEach((key) => {
+    if (key in obj) {
+      clone[key] = obj[key];
+    }
+  });
+  return clone;
+}
