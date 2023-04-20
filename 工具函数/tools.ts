4ccb77e8c3c2efb57e83e167463b1787f5350e04
zhaoyebaitu <122477559@qq.com>
2023-04-20 19:11:53 +0800

    feat: 防抖、节流

--- 工具函数/tools.ts
+++ 工具函数/tools.ts
@@ -19,7 +19,6 @@ function omit<T extends Record<string | number, any>, K extends keyof T>(
   return clone;
 }
 
-//
 /**
  * @description: 筛选出对象中需要的属性，返回由筛选出的属性组成的对象的浅拷贝
  * @param {T} obj
@@ -34,4 +33,50 @@ function pick<T extends Record<string | number, any>, K extends keyof T>(obj: T,
     }
   });
   return clone;
+}
+
+/**
+ * @description: 防抖
+ * @param {Function} fn
+ * @param {number} timeout
+ * @param {*} timeout
+ * @return {*}
+ */
+function debounce(fn: Function, timeout: number) {
+  let timer: any = null;
+  // 利用闭包存储timer，不然每次重新执行函数，timer会被重置，无法起到判断的作用
+  // 在没有递归调用的情况下，执行完成的setTimeout不需要使用clearTimeout清理
+  return function (this: any, ...args: any[]) {
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      typeof fn === "function" && fn.apply(this, args);
+    }, timeout);
+  };
+}
+
+/**
+ * @description: 节流
+ * @param {Function} fn
+ * @param {number} timeout
+ * @param {boolean} immediate
+ * @param {*} timeout
+ * @param {*} timeout
+ * @return {*}
+ */
+function throttle(fn: Function, timeout: number, immediate: boolean = true) {
+  let timer: any = null;
+  return function (this: any, ...args: any[]) {
+    if (timer) return;
+    if (immediate) {
+      typeof fn === "function" && fn.apply(this, args);
+      timer = setTimeout(() => {
+        timer = null;
+      }, timeout);
+    } else {
+      timer = setTimeout(() => {
+        typeof fn === "function" && fn.apply(this, args);
+        timer = null;
+      }, timeout);
+    }
+  };
 }
