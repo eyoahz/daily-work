b1b4c1ed23f0ac41c39bcf6c70403f64901cdbd6
zhaoyebaitu <122477559@qq.com>
2023-04-23 19:25:15 +0800

    feat: vue-test

--- vue-test/src/views/HomeView.vue
+++ vue-test/src/views/HomeView.vue
@@ -1,9 +1,49 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// 防抖：规定事件内重复触发会重新计时
+function debounce(fn: () => any, timeout: number = 500, immediate: boolean = false) {
+  let timer: any = null
+  return function (this: any, ...args: any) {
+    timer && clearTimeout(timer)
+    if (immediate) {
+      !timer && typeof fn === 'function' && fn.apply(this, args)
+      timer = setTimeout(() => {
+        timer = null
+      }, timeout)
+    } else {
+      timer = setTimeout(() => {
+        typeof fn === 'function' && fn.apply(this, args)
+        timer = null
+      }, timeout)
+    }
+  }
+}
+
+// 节流：规定时间内只承认一次操作
+function throttle(fn: () => any, timeout: number, immediate: boolean = true) {
+  let timer: any = null
+  return function (this: any, ...args: any) {
+    if (timer) return
+    if (immediate) {
+      typeof fn === 'function' && fn.apply(this, args)
+      timer = setTimeout(() => {
+        timer = null
+      }, timeout)
+    } else {
+      timer = setTimeout(() => {
+        typeof fn === 'function' && fn.apply(this, args)
+        timer = null
+      }, timeout)
+    }
+  }
+}
+
+const handleClick1 = debounce(() => console.log('点击'), 1000, true)
+const handleClick2 = throttle(() => console.log('点击'), 1000, false)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <button @click="handleClick1">防抖</button>
+    <button @click="handleClick2">节流</button>
   </main>
 </template>
