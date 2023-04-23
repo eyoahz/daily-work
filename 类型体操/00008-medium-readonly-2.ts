6ee7a84e67396548ada16b89216442ddbfc32fc2
zhaoyebaitu <122477559@qq.com>
2023-04-23 16:14:26 +0800

    feat: 类型挑战

--- 类型体操/00008-medium-readonly-2.ts
+++ 类型体操/00008-medium-readonly-2.ts
@@ -34,4 +34,7 @@ type MyExclude<T, K> = T extends K ? never : T;
 // 即MyReadonly2<T> 等价于 MyReadonly2<T, keyof T>
 type MyReadonly2<T, K extends keyof T = keyof T> = { readonly [P in K]: T[P] } & { [P in MyExclude<keyof T, K>]: T[P] };
 
+// 第二种解法
+// type MyReadonly2<T, K extends keyof T = keyof T> = { readonly [P in K]: T[P] } & { [P in keyof T as P extends K ? never : P]: T[P] };
+// 第三种解法
 // type MyReadonly2<T, K extends keyof T = keyof T> = { readonly [P in K]: T[P] } & Omit<T, K>
