83e6f1a3b8292fa9414c8d52fc7dec93cab875d5
zhaoye <122477559@qq.com>
2023-05-30 11:36:54 +0800

    feat: 类型挑战

---
+++ 类型体操/00599-medium-merge.ts
@@ -0,0 +1,41 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type Foo = {
+  a: number;
+  b: string;
+};
+type Bar = {
+  b: number;
+  c: boolean;
+};
+
+type cases = [
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number;
+        b: number;
+        c: boolean;
+      }
+    >
+  >
+];
+
+// ============= Your Code Here =============
+/* Region 不优雅 */
+// 可以把 MyOmit 换成TS自带的 Omit
+// type MyOmit<T extends Record<string | number | symbol, any>, U> = { [K in keyof T as K extends U ? never : K]: T[K] };
+// type Merge<F extends Record<string | number | symbol, any>, S extends Record<string | number | symbol, any>> = {
+//   [K in keyof MyOmit<F, keyof S> | keyof S]: K extends keyof S ? S[K] : F[K];
+// };
+/* End 不优雅 */
+
+type Merge<F extends Record<string | number | symbol, any>, S extends Record<string | number | symbol, any>> = {
+  [K in keyof (F & S)]: K extends keyof S ? S[K] : F[K];
+};
+
+// 注意两个 Test预览 的差别
+type Test1 = keyof Foo & Bar;
+type Test2 = keyof (Foo & Bar);
