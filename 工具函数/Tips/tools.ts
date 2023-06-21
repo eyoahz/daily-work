4d93f1168da368557ef4a2520399719543a91f2d
zhaoye <122477559@qq.com>
2023-06-21 16:34:01 +0800

    feat: 工具函数更新

--- 工具函数/Tips/tools.ts
+++ 工具函数/Tips/tools.ts
@@ -2,7 +2,7 @@
  * @description: 随机十六进制颜色值
  * @return {*}
  */
-const randomHexColor: () => string = (): any =>
+const randomHexColor: () => string = () =>
   `#${Math.floor(Math.random() * 0xffffff)
     .toString(16)
     .padEnd(6, "0")}`;
@@ -11,4 +11,14 @@ const randomHexColor: () => string = (): any =>
  * @description: 获取用户选择的文本
  * @return {*}
  */
-const getSelectedText: () => string | undefined = (): any => window.getSelection()?.toString();
+const getSelectedText: () => string | undefined = () => window.getSelection()?.toString();
+
+/**
+ * @description: 将 RGB 转换为十六进制
+ * @param {number} r
+ * @param {number} g
+ * @param {number} b
+ * @return {*}
+ */
+const rgbToHex: (r: number, g: number, b: number) => string = (r, g, b) =>
+  `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
