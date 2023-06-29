861e906388f6f51753ffd20d96e2e117cc51f2af
zhaoye <122477559@qq.com>
2023-06-29 17:40:29 +0800

    feat: 工具函数更新

--- 工具函数/数组相关操作/tools.ts
+++ 工具函数/数组相关操作/tools.ts
@@ -12,3 +12,20 @@ function arrReverse<T>(arr: Array<T>, start: number, end: number) {
     end--;
   }
 }
+
+/**
+ * @description: 将数组中的元素向右轮转 k 个位置，其中 k 是非负数 注：该方法会改变源数组
+ * @param {Array} arr
+ * @param {number} k
+ * @return {*}
+ */
+function arrRotate<T>(arr: Array<T>, k: number): void {
+  const length: number = arr.length;
+  if (k % length === 0) return;
+
+  k %= length;
+
+  arrReverse(arr, 0, length - 1);
+  arrReverse(arr, 0, k - 1);
+  arrReverse(arr, k, length - 1);
+}
