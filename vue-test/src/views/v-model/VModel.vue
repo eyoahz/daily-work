34a0c039c5756fbab46f2f3f4ee4973e691d241a
zhaoye <122477559@qq.com>
2023-07-07 09:17:54 +0800

    feat: vue-test

---
+++ vue-test/src/views/v-model/VModel.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps<{
+    show: boolean
+}>();
+
+const emits = defineEmits(['update:show'])
+
+const showRef = ref<boolean>(false);
+
+const showFun = watch(() => props.show, () => {
+    console.log('触发！！！！！')
+})
+
+</script>
+
+<template>
+  <el-dialog :model-value="props.show" @close="emits('update:show', false)"></el-dialog>
+</template>
+
+<style scoped lang="scss">
+
+</style>
