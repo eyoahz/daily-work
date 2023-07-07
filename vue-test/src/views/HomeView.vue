d7bc82eb24bd30e465942ecba1d80ba77b3b08c6
zhaoye <122477559@qq.com>
2023-07-07 09:17:49 +0800

    feat: vue-test

--- vue-test/src/views/HomeView.vue
+++ vue-test/src/views/HomeView.vue
@@ -1,67 +1,20 @@
 <script setup lang="ts">
-import { reactive, onMounted, ref } from "vue";
+import VModel from '@/views/v-model/VModel.vue'
+import { ref } from 'vue'
 
-const name = ref<string>('');
-const data = reactive<Record<any, any>>({
-    a: 123
-})
+const show = ref<boolean>(false);
 
-// 防抖：规定事件内重复触发会重新计时
-function debounce(fn: () => any, timeout: number = 500, immediate: boolean = false) {
-  let timer: any = null
-  return function (this: any, ...args: any) {
-    timer && clearTimeout(timer)
-    if (immediate) {
-      !timer && typeof fn === 'function' && fn.apply(this, args)
-      timer = setTimeout(() => {
-        timer = null
-      }, timeout)
-    } else {
-      timer = setTimeout(() => {
-        typeof fn === 'function' && fn.apply(this, args)
-        timer = null
-      }, timeout)
-    }
-  }
+const handle = (flag: boolean) => {
+    show.value = flag;
 }
 
-// 节流：规定时间内只承认一次操作
-function throttle(fn: () => any, timeout: number, immediate: boolean = true) {
-  let timer: any = null
-  return function (this: any, ...args: any) {
-    if (timer) return
-    if (immediate) {
-      typeof fn === 'function' && fn.apply(this, args)
-      timer = setTimeout(() => {
-        timer = null
-      }, timeout)
-    } else {
-      timer = setTimeout(() => {
-        typeof fn === 'function' && fn.apply(this, args)
-        timer = null
-      }, timeout)
-    }
-  }
-}
-
-const variable = ref<number>(0);
-const handleClick1 = () => {
-    variable.value++;
-    console.log(123)
-}
-const handleClick2 =
-
-onMounted(() => {
-  document.querySelectorAll('button')!.forEach(node => {
-      node.innerHTML = '<div>傻逼3</div>'
-  })
-})
 </script>
 
 <template>
   <main>
-      {{ variable }}
-    <button @click="handleClick1">防抖</button>
-    <button @click="handleClick2">节流</button>
+    <button @click="() => handle(true)">防抖</button>
+    <button @click="() => handle(false)">节流</button>
+    <!--<VModel v-model:show="show"></VModel>-->
+    <VModel v-model:show="show"></VModel>
   </main>
 </template>
