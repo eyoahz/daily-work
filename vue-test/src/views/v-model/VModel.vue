e2742e7ced1b1ee1609116bf078bee7240d1aff4
zhaoye <122477559@qq.com>
2023-07-13 14:15:54 +0800

    feat: vue-test

--- vue-test/src/views/v-model/VModel.vue
+++ vue-test/src/views/v-model/VModel.vue
@@ -1,24 +1,29 @@
 <script lang="ts" setup>
-import { makeList } from 'element-plus'
 import { reactive } from 'vue'
 
 const data = reactive<Record<string, any>>({
   name: '小黑',
   age: 18,
   sex: 'male',
-  hobby: 'soccer'
+  hobby: 'soccer',
 })
 </script>
 
 <template>
-  <div>测试具名插槽</div>
-  <slot name="test" :data="data"></slot>
-  <slot :data="data"></slot>
+  <div>测试具名插槽测试具名插槽测试具名插槽测试具名插槽测试具名插槽测试具名插槽测试具名插槽测试具名插槽</div>
+  <slot name="test" :data="data" />
+  <slot :data="data" />
 
   <el-tabs>
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="User" name="first">
+      User
+    </el-tab-pane>
+    <el-tab-pane label="Config" name="second">
+      Config
+    </el-tab-pane>
+    <el-tab-pane label="Role" name="third">
+      Role
+    </el-tab-pane>
   </el-tabs>
 </template>
 
