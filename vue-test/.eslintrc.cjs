bcf25314bf2b64a776cb0fd234d286734c8ee134
zhaoye <122477559@qq.com>
2023-07-14 09:20:31 +0800

    feat: vue-test

--- vue-test/.eslintrc.cjs
+++ vue-test/.eslintrc.cjs
@@ -1,5 +1,6 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+// require('@rushstack/eslint-patch/modern-module-resolution')
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
 
 module.exports = {
   root: true,
@@ -7,7 +8,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    '@antfu'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
