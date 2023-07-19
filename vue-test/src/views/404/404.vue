d9677d38593ace1cdcacb1931aaa28201931ef42
zhaoye <122477559@qq.com>
2023-07-19 09:18:01 +0800

    feat: vue-test

---
+++ vue-test/src/views/404/404.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+console.log(useRoute());
+
+</script>
+
+<template>
+  <div class="box">404</div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 400px;
+  aspect-ratio: 1;
+  background-image: linear-gradient(#ff0 33%, #00f 0% 66%, #f0f 0%);
+}
+</style>
