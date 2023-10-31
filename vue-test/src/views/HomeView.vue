185f98377962678b5161d53bed4f15e11f193f45
zhaoye <zhaoyebaitu@gmail.com>
2023-10-31 09:18:40 +0800

    style: none

--- vue-test/src/views/HomeView.vue
+++ vue-test/src/views/HomeView.vue
@@ -1,27 +1,13 @@
 <script setup lang="ts">
-import VModel from '@/views/v-model/VModel.vue'
 import { ref } from 'vue'
+import Child from './Child.vue';
 
-const show = ref<boolean>(false)
-
-const handle = (flag: boolean) => {
-  show.value = flag
-  console.log('触发')
-}
+const show = ref<boolean>(true)
 </script>
 
 <template>
   <main>
-    <button @click="() => handle(true)">防抖</button>
-    <button @click="() => handle(false)">节流</button>
-    <!--<VModel v-model:show="show"></VModel>-->
-    <VModel>
-      <template v-slot="{ data }">
-        {{ data.hobby }}
-      </template>
-      <template #test="{ data }">
-        {{ data.name + data.age }}
-      </template>
-    </VModel>
+    {{ show }}
+    <Child v-model:show="show"></Child>
   </main>
 </template>
